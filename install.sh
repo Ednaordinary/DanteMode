1d0f4f85380aeb1b11c485d1e10ac6fd488f4c5f
Ednaordinary <88869424+Ednaordinary@users.noreply.github.com>
2021-10-15 12:10:28 -0600

    !NOT TESTED AT ALL! install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+read -rsp $'Press any key to verify you are running this script in the same location that you cloned this repo to (you should be in /DanteMode/)\n' -n1 key
+echo Please install and configure ffmpeg and docker now by doing the following commands:
+echo sudo apt install ffmpeg docker.io
+echo sudo groupadd docker
+echo sudo usermod -aG docker $USER
+read -rsp $'Press any key to continue the setup\n' -n1 key
+newgrp docker
+python3 -m pip install -U big-sleep
+python3 -m pip install -U nextcord
+python3 -m pip install -U gettext
+python3 -m pip install -U python-dotenv
+python3 -m pip install -U shutil
+python3 -m pip install -U numpy
+python3 -m pip install -U torch=1.7.1
+python3 -m pip install -U pillow
+python3 -m pip install -U deep_daze
+docker pull alexjc/neural-enhance
+echo Please install CUDA now
+echo https://developer.nvidia.com/cuda-downloads
+echo Common mistake: if your on ubuntu, use the ubuntu install, NOT the debian one
+read -rsp $'Press any key to continue the setup\n' -n1 key
+touch ./.env
+echo "DISCORD_TOKEN=" >> ./.env
+echo Add you discord token in .env so it looks like DISCORD_TOKEN=* where * is your discord token
+read -rsp $'Press any key to finish the setup\n' -n1 key
