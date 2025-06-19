97c78063938f4d43b8c85625999256c3f807e384
BuildTools <imnotednamode@gmail.com>
2025-06-19 06:07:51 -0600

    make stuff work

--- orchestrator.py
+++ orchestrator.py
@@ -1,37 +1,81 @@
 import modal
-import asyncio
-
-app = modal.App("Dante")
-image = modal.Image \
-    .from_registry("pytorch/pytorch:2.7.1-cuda11.8-cudnn9-runtime") \
-    .pip_install(
-    "git+https://github.com/huggingface/diffusers",
-    "transformers",
-    "accelerate",
-    "sentencepiece",
-    "protobuf",
-    "hf_transfer",
-) \
-    .pip_install(
-    "flash-attn", extra_options="--no-build-isolation"
-) \
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-volume = modal.Volume.from_name("dante-models", create_if_missing=True)
-model_dir = "/model"
-hf_token = modal.Secret.from_name(
-    "hf-token", required_keys=["HF_TOKEN"]
-)
+import threading
+from queue import Queue
 
 
-@app.function(image=image, volumes={"/model": volume}, secrets=[hf_token])
-async def download(self, path, kwargs):
-    from diffusers import DiffusionPipeline
-    DiffusionPipeline.from_pretrained(path, **kwargs)
+class Function:
+    def __init__(self, function, *args, **kwargs):
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
 
 
-@app.function(gpu="T4", image=image, volumes={"/model": volume})
-async def get_embeds(self, pipeline):
-    pass
+class End:
+    def __init__(self, queue):
+        self.queue = queue
 
 
-app.run()
+def main(queue):
+    app = modal.App("Dante")
+    image = modal.Image \
+        .from_registry("nvidia/cuda:12.9.0-cudnn-runtime-ubuntu24.04") \
+        .apt_install("git") \
+        .pip_install(
+        "git+https://github.com/huggingface/diffusers",
+        "transformers",
+        "accelerate",
+        "sentencepiece",
+        "protobuf",
+        "hf_transfer",
+        "torch",
+        "numpy",
+    ) \
+        .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    volume = modal.Volume.from_name("dante-models", create_if_missing=True)
+    model_dir = "/model"
+    hf_token = modal.Secret.from_name(
+        "hf-token", required_keys=["HF_TOKEN"]
+    )
+
+    slim_image = modal.Image \
+        .debian_slim() \
+        .pip_install("huggingface_hub", "torch")
+
+    @app.function(image=slim_image, volumes={"/model": volume}, secrets=[hf_token], serialized=True, retries=0, memory=1024*1, timeout=60*60)
+    def download(model, path, **kwargs):
+        print("Importing downloader")
+        import torch
+        from huggingface_hub import snapshot_download
+        print("Starting download")
+        snapshot_download(
+            model,
+            local_dir="/model/" + path,
+            ignore_patterns=["*.pt", "*.bin"],  # using safetensors
+        )
+        print("Download complete")
+        volume.commit()
+
+    @app.function(gpu="T4", image=image, volumes={"/model": volume}, serialized=True, retries=0)
+    async def get_embeds(pipeline):
+        pass
+
+    def wait_for_end(queue, calls):
+        for i in calls:
+            print(i.get())
+        queue.put(1)
+
+    with modal.enable_output():
+        with app.run():
+            calls = []
+            while True:
+                item: Function = queue.get()
+                if isinstance(item, End):
+                    wait_for_end(item.queue, calls)
+                    break
+                calls.append(locals()[item.function].spawn(*item.args, **item.kwargs))
+
+
+def start():
+    queue = Queue()
+    threading.Thread(target=main, args=[queue]).start()
+    return queue
--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-import asyncio
 import torch
+from queue import Queue
 
 import orchestrator
 
+queue = orchestrator.start()
+
 downloads = [
-    ["black-forest-labs/FLUX.1-dev", {"torch_dtype": torch.bfloat16}],
-    ["black-forest-labs/FLUX.1-schnell", {"text_encoder": None, "text_encoder_2": None, "torch_dtype": torch.bfloat16}]
+    ["black-forest-labs/FLUX.1-dev", "flux-dev", {"torch_dtype": torch.bfloat16}],
+    ["black-forest-labs/FLUX.1-schnell", "flux-dev", {"text_encoder": None, "text_encoder_2": None, "torch_dtype": torch.bfloat16}]
 ]
 
 
-async def setup():
-    for i in downloads:
-        await asyncio.gather(*[orchestrator.download(i[0], i[1]) for i in downloads])
+for i in downloads:
+    function = orchestrator.Function("download", i[0], i[1], **i[2])
+    queue.put(function)
 
-
-loop = asyncio.new_event_loop()
-loop.run_until_complete(setup())
+end = Queue()
+queue.put(orchestrator.End(end))
+end.get()
